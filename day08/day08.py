5c3d535744c22a53bd537267eadc9bf85f3e460c
Brian <33679241+brianfranklin21@users.noreply.github.com>
2022-12-09 12:18:41 -0800

    part2

--- day08/day08.py
+++ day08/day08.py
@@ -35,18 +35,61 @@ class DayEight:
                 if ut < self.df0.loc[row, col]:
                     ut = self.df0.loc[row, col]
                     self.df1.loc[row, col] = 1
-            bt = -1
+            dt = -1
             for row in self.df0.index[::-1]:
-                if bt < self.df0.loc[row, col]:
-                    bt = self.df0.loc[row, col]
+                if dt < self.df0.loc[row, col]:
+                    dt = self.df0.loc[row, col]
                     self.df1.loc[row, col] = 1
-        print(self.df1)
+        # display(self.df1)
         print(self.df1.sum().sum())
 
+    def score(self, row, col):
+        max_col = self.df0.columns[-1]
+        max_row = self.df0.index[-1]
+        # look left
+        ls = 0
+        if col > 0:
+            v = self.df0.loc[row, 0:col].tolist()[::-1]
+            for i in v[1:]:
+                ls += 1
+                if i >= v[0]:
+                    break
+        # look right
+        rs = 0
+        if col < max_col:
+            v = self.df0.loc[row, col:].tolist()
+            for i in v[1:]:
+                rs += 1
+                if i >= v[0]:
+                    break
+        # look up
+        us = 0
+        if row > 0:
+            v = self.df0.loc[0:row, col].tolist()[::-1]
+            for i in v[1:]:
+                us += 1
+                if i >= v[0]:
+                    break
+        # look down
+        ds = 0
+        if row < max_row:
+            v = self.df0.loc[row:, col].tolist()
+            for i in v[1:]:
+                ds += 1
+                if i >= v[0]:
+                    break
+        return ls * rs * us * ds
+
     def part2(self):
+        """ What is the highest scenic score possible for any tree?
         """
-        """
-        print()
+        self.df2 = self.df0.copy()
+        self.df2.loc[:, :] = 0
+        for row in self.df0.index:
+            for col in self.df0.columns:
+                self.df2.loc[row, col] = self.score(row, col)
+        print(self.df2)
+        print(self.df2.max().max())
 
 
 # day8 = DayEight("sample.txt")
