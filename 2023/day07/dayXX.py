74b35015c5c757852bfcaaa22d54e8970fcfce92
Brian <33679241+brianfranklin21@users.noreply.github.com>
2023-12-05 22:57:28 -0800

    pre-populate up to day10

---
+++ 2023/day07/dayXX.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+https://adventofcode.com/2023/day/XX
+Author: Brian Franklin
+"""
+
+
+class DayXX:
+    """Class for solving the puzzle"""
+
+    def __init__(self, filename: str) -> None:
+        """Parse the puzzle input text and calculate the answers."""
+        with open(filename, "r", encoding="utf-8") as f:
+            # self.data = [line.split() for line in f]
+            self.data = list(f)
+        # print(self.data)
+        self.solve_part1()
+        self.solve_part2()
+
+    def solve_part1(self) -> None:
+        """Solve the puzzle for part 1."""
+        res = [0]
+        # print(res)
+        self.part1 = sum(res)
+
+    def solve_part2(self) -> None:
+        """Solve the puzzle for part 2."""
+        res2 = [0]
+        # print(res2)
+        self.part2 = sum(res2)
+
+    def __str__(self) -> str:
+        """Return the puzzle answers as a string."""
+        return f"{type(self).__name__}(part1={self.part1!r}, part2={self.part2!r})"
+
+
+Test = DayXX("sample.txt")
+assert Test.part1 == 0, f"Expected 0, got {Test.part1}"
+# assert Test.part2 == 0, f"Expected 0, got {Test.part2}"
+
+# Puzzle = DayXX("input.txt")
+# print(Puzzle)
+# assert Puzzle.part1 == 0, f"Expected 0, got {Puzzle.part1}"
+# assert Puzzle.part2 == 0, f"Expected 0, got {Puzzle.part2}"
