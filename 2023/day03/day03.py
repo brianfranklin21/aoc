983bc56ad21684af0c25fd29feb9d0d587a9c101
Brian <33679241+brianfranklin21@users.noreply.github.com>
2023-12-05 22:19:17 -0800

    complete day03

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -3,7 +3,8 @@
 https://adventofcode.com/2023/day/3
 Author: Brian Franklin
 """
-import re
+import re, math
+from collections import defaultdict
 
 
 class Day03:
@@ -22,34 +23,36 @@ class Day03:
         r_max = len(self.data)
         c_max = len(self.data[0]) - 1
 
-        symbols = {
+        self.symbols = {
             (r, c): self.data[r][c]
             for r in range(r_max)
             for c in range(c_max)
             if self.data[r][c] not in "0123456789."
         }
 
-        numbers = {
+        self.numbers = {
             (r, (n.start(), n.end())): int(n.group())
             for r, row in enumerate(self.data)
             for n in re.finditer(r"\d+", row)
         }
 
-        part_numbers = {}
-        for (r, (c1, c2)), v in numbers.items():
+        self.part_numbers = defaultdict(list)
+        for (r, (c1, c2)), v in self.numbers.items():
             edge = {(r, c) for r in (r - 1, r, r + 1) for c in range(c1 - 1, c2 + 1)}
-            for _ in edge & symbols.keys():
-                part_numbers[r, c1] = v
+            for rs, cs in edge & self.symbols.keys():
+                self.part_numbers[rs, cs].append(v)
 
-        # print(symbols)
-        # print(numbers)
-        # print(part_numbers)
-        self.part1 = sum(part_numbers.values())
+        # print(self.symbols)
+        # print(self.numbers)
+        # print(self.part_numbers)
+        pn_flat_list = sum(self.part_numbers.values(), [])  # using sum as list concat
+        self.part1 = sum(pn_flat_list)
 
     def solve_part2(self) -> None:
         """Solve the puzzle for part 2."""
-        # print(cal_data2)
-        self.part2 = sum([0])
+        self.part2 = sum(
+            math.prod(p) for p in self.part_numbers.values() if len(p) == 2
+        )
 
     def __str__(self) -> str:
         """Return the puzzle answers as a string."""
@@ -58,9 +61,9 @@ class Day03:
 
 Test = Day03("sample.txt")
 assert Test.part1 == 4361, f"Expected 4361, got {Test.part1}"
-# assert Test.part2 == 0, f"Expected 0, got {Test.part2}"
+assert Test.part2 == 467835, f"Expected 467835, got {Test.part2}"
 
 Puzzle = Day03("input.txt")
 print(Puzzle)
 assert Puzzle.part1 == 553079, f"Expected 553079, got {Puzzle.part1}"
-# assert Puzzle.part2 == 0, f"Expected 0, got {Puzzle.part2}"
+assert Puzzle.part2 == 84363105, f"Expected 84363105, got {Puzzle.part2}"
